a98744c6a53d7132f050e2c60d81dec8be3ce8b3
Raccoon2508 <ridge2006@mail.ru>
2019-03-18 14:55:19 +0300

    feat: passing task love-triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  arr=preferences;
+
+var redArr=[];
+for(var i=0; i<arr.length; i++)
+{
+  redArr[i+1]=arr[i];
+}
+
+var count=0
+
+for(var i=0; i<=redArr.length; i++)
+if(i!=redArr[i]&&i!=redArr[redArr[i]]&&redArr[i]!=redArr[redArr[i]]){
+  if(redArr[redArr[redArr[i]]]==i){
+    count++;
+    redArr[redArr[redArr[i]]]=null;
+    redArr[redArr[redArr[i]]]=null;
+    redArr[redArr[redArr[i]]]=null;
+    }
+}
+return count;// your implementation
 };
